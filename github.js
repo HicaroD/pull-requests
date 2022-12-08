3c250bb38552053c202ed7e1361bbc5efdf97533
HicaroD <hdanrlley1@gmail.com>
2022-12-08 10:36:32 -0300

    feature(errorReporter): properly reporting error to the user

--- github.js
+++ github.js
@@ -1,5 +1,18 @@
 const axios = require("axios");
 
+const reportError = (response) => {
+    if(!response) {
+	console.log("Unexpected error!");
+    }
+
+    let status_code = response.status;
+    if(status_code === 422) {
+	console.log("Make sure there are changes in order to make a PR.");
+    } else {
+	console.log("Forbidden! Have you passed the right credentials?");
+    }
+}
+
 const postPullRequest = async ({title, base_branch, head_branch, configuration}) => {
     if (title === undefined || base_branch === undefined) {
 	console.log("Title and/or base branch should not be empty");
@@ -17,14 +30,16 @@ const postPullRequest = async ({title, base_branch, head_branch, configuration})
     };
     let endpoint_url = `https://api.github.com/repos/${configuration.username}/${configuration.repository}/pulls`;
 
-    let response = await axios
+    await axios
 	.post(
 	    endpoint_url,
 	    body,
 	    { headers: headers }
         )
-	.then(({data}) => {
-	   console.log(data)
+	.then(() => {
+	    console.log(`Pull request from "${head_branch}" was successfuly created on "${base_branch}"`)
+	}, (error) => {
+	    reportError(error.response);
 	}
     );
 }
