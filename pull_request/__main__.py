5cdc044a3853bec249532b645f4bfc64f6f171c7
HicaroD <hdanrlley1@gmail.com>
2022-12-20 11:24:06 -0300

    structure: removing unnecessary file
    i'm moving the code from `main.py` to `__main__.py`

--- pull_request/__main__.py
+++ pull_request/__main__.py
@@ -1,5 +1,46 @@
-import asyncio 
-from main import main
+import argparse
+import json
+import asyncio
+from pullrequest import PullRequest, Credentials, PullRequestData
+
+
+def get_argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Pull requests on command line - Author: Icarus"
+    )
+    parser.add_argument(
+        "-f", "--from", help="Changes are coming from this branch", required=True
+    )
+    parser.add_argument(
+        "-t", "--to", help="Changes are going to that branch", required=True
+    )
+    parser.add_argument("-T", "--title", help="Pull request title", required=True)
+    parser.add_argument("-b", "--body", help="Body of the pull request")
+    return parser
+
+
+def read_credentials() -> Credentials:
+    with open("pr.json") as file:
+        content = file.read()
+        parsed_content = json.loads(content)
+        # TODO: add error handling in case of KeyError
+        return Credentials(
+            parsed_content["username"],
+            parsed_content["repository"],
+            parsed_content["token"],
+        )
+
+
+async def main():
+    argument_parser = get_argument_parser()
+    args = vars(argument_parser.parse_args())
+
+    pr_content = PullRequestData(args["from"], args["to"], args["title"], args["body"])
+    credentials = read_credentials()
+    pr = PullRequest(credentials, pr_content)
+    await pr.make_pull_request()
+
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    raise SystemExit(asyncio.run(main()))
