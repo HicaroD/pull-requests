a48ca7a6cbd826759d87fe086d86b6658ada30be
HicaroD <hdanrlley1@gmail.com>
2022-12-25 14:24:01 -0300

    proj: changing version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as f:
 
 setup(
    name='pull-requests',
-   version='1.0.0',
+   version='1.1.0',
    description='A command line interface for creating pull requests',
    long_description=long_description,
    long_description_content_type="text/markdown",
