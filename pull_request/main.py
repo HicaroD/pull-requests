9ce2962d516ead9a8e965158b4ecc193c3c43a84
HicaroD <hdanrlley1@gmail.com>
2023-02-25 10:43:42 -0300

    fix: handling `CredentialFileDoesNotExist` exception

--- pull_request/main.py
+++ pull_request/main.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pull_request.command_line_argument_parser import CommandLineArgumentParser
 from pull_request.credential_manager import CredentialManager
-from pull_request.exceptions import InvalidFormatForCredentialFile
+from pull_request.exceptions import CredentialFileDoesNotExist, InvalidFormatForCredentialFile
 from pull_request.pullrequest import PullRequest, PullRequestData
 
 
@@ -25,6 +25,9 @@ async def main():
         print(
             "Error: Make sure your 'pr.json' has all required files: 'username', 'repository' and 'token'."
         )
+    except CredentialFileDoesNotExist:
+        print("Error: Make sure your 'pr.json' file exists in the root directory of your project.")
+        
 
 
 def _main():
