f0faaad949a8f6df87739f4dae1ebf9eedec3620
HicaroD <hdanrlley1@gmail.com>
2023-02-25 10:44:14 -0300

    fmt: `black`

--- pull_request/main.py
+++ pull_request/main.py
@@ -2,7 +2,10 @@ import asyncio
 
 from pull_request.command_line_argument_parser import CommandLineArgumentParser
 from pull_request.credential_manager import CredentialManager
-from pull_request.exceptions import CredentialFileDoesNotExist, InvalidFormatForCredentialFile
+from pull_request.exceptions import (
+    CredentialFileDoesNotExist,
+    InvalidFormatForCredentialFile,
+)
 from pull_request.pullrequest import PullRequest, PullRequestData
 
 
@@ -26,8 +29,9 @@ async def main():
             "Error: Make sure your 'pr.json' has all required files: 'username', 'repository' and 'token'."
         )
     except CredentialFileDoesNotExist:
-        print("Error: Make sure your 'pr.json' file exists in the root directory of your project.")
-        
+        print(
+            "Error: Make sure your 'pr.json' file exists in the root directory of your project."
+        )
 
 
 def _main():
